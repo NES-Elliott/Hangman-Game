8ca2f20096ff1cd15e7d07637d7dc0ab4e4ea574
Nathaniel Spry <nspry1997@gmail.com>
2017-09-05 21:30:40 -0400

    added the newGame function, fixed HTML left and right columns

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,48 @@
 $(document).ready(function(){
 	// ~~~VARIABLES~~~
-	// wins
-	// losses
-	// lettersGuessed (array)
-	// wordBank (array)
-	// guessesLeft
-	// pickedWord
-	// pickedWordArray
-	// pickedWordPlaceholders
+	var wins;
+	var losses;
+	var guessesLeft = 10;
+	var pickedWord;
+	var pickedWordPlaceholders;
+
+	var pickedWordArray = [];
+	var lettersGuessed = ["h", "e", "l", "l", "o"];
+	var wordBank = ["hello", "jello", "yellow", "fellow"];
+
+	// ~~~SELECTORS~~~
+	var newGameBtn = $("#new-game")
+	var winsDiv = $("#wins")
+	var lossDiv = $("#losses")
+	var guessLeftDiv = $("#guesses-left")
+	var blankSpaceDiv = $("#blank-spaces")
+	var lettersGuessedDiv = $("#letters-guessed")
 
 	// ~~~FUNCTIONS~~~
 	// New Game Function ----- 1
+	function newGame() {
 		// reset guessesLeft
-		// empty out pickedWordPlaceholders
-		// empty out lettersGuessed (both array and DOM)
+		guessesLeft = 10;
+		pickedWordPlaceholders = [];
+		lettersGuessed = [];
 
-		// change pickedWord (using Math.floor(Math.random() * array.length)))
-		// split pickedWord into an array (array.split(""))
-		// loop over pickedWordArray and create blank dashes for over placeholders
+		pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+		pickedWordArray = pickedWord.split("");
 
-		// placeholders.join(" ") to the DOM
+		for (var i = 0; i < pickedWordArray.length; i++) {
+			if (pickedWordArray[i] === " ") {
+				pickedWordPlaceholders.push(" ");
+			} else {
+				pickedWordPlaceholders.push("_");
+			}
+		}
 
+		lettersGuessedDiv.html("");
+		blankSpaceDiv.html("");
+		blankSpaceDiv.text(pickedWordPlaceholders.join(" "));
+	}
 	// letterPressed(takes one arg) ----- 2
+	function letterPressed(letter) {
 		// run a for loop over our pickedWordArray
 			// if valOfLetterPressed === pickedWordArray[i]
 				// it's a match, placeholdersArray[i] = valOfLetterPressed
@@ -31,9 +52,17 @@ $(document).ready(function(){
 				// guessesLeft --
 				// if guessesLeft === 0, alert the player the game is over
 					//write Game Over message to DOM and let them know the correct word
+	}
 
 	// ~~~EVENT LISTENERS~~~
 	// NEW GAME BUTTON ----- 1
+	newGameBtn.on("click", function() {
+		console.log("working");
+		newGame();
+		console.log("pickedWord: " + pickedWord);
+		console.log(pickedWordArray);
+		console.log(pickedWordPlaceholders);
+	})
 		// when button is pressed, run new game function
 
 	// KEYUP LISTENER ----- 2
@@ -45,7 +74,7 @@ $(document).ready(function(){
 		// if keyVal is already in lettersGuessedArray
 			// if NOT push keyVal into lettersGuessed Array
 			// letterPress(keyVal)
-			
+
 		// else, let them know the letter was already guessed
 	// })
 
