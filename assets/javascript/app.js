8d14efb2ebc33a8434bdbc4dd374b4d5038dc115
Nathaniel Spry <nspry1997@gmail.com>
2018-03-04 01:32:34 -0500

    basic psuedo code in comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
+/*
+
+~~~ PSUEDO CODE ~~~
+
+PlayGame, Restart, PlayAgain Buttons - Starts a new game for the player
+
+Word is picked from a bank of words at random and stored in a variable
+
+Player clicks a key on their keyboard that cooresponses with the letter they would like to guess
+(ONLY ALLOW LETTERS AND DASHES)
+
+If the word is guessed correctly, increment the players win score by 1
+If the word is guessed incorrectly and the player has no more guesses, increment the loss score by 1
+
+*/
+
+
+
+
 $(document).ready(function(){
   console.log("ready")
 })
